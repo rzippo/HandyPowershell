eae203a30f6dffc32b0b20934e4500c37534fdc1
Raffaele Zippo <raffaele.zippo@outlook.it>
2017-08-15 16:49:31 +0200

    Refactoring as executor

--- executorTest/buildExpressionsAndCallExecutor.ps1
+++ executorTest/buildExpressionsAndCallExecutor.ps1
@@ -1,5 +1,5 @@
 $target = "E:\Users\Raffaele\Desktop\test";
-$executionTarget = "E:\Users\Raffaele\Documents\GitHub\HandyPowershell\executeTest\executionTarget.ps1";
+$executionTarget = "E:\Users\Raffaele\Documents\GitHub\HandyPowershell\executorTest\executionTarget.ps1";
 $numFiles = 30;
 $nPar = 5;
 $sleepTime = 3;
@@ -12,4 +12,4 @@ foreach( $i in (1..$numFiles) )
 	$expressions.Add("$executionTarget -pwd $target -filename $filename -sleepTime $sleepTime") | Out-Null;
 }
 
-E:\Users\Raffaele\Documents\GitHub\HandyPowershell\executeExpressions.ps1 -nParallelJobs $nPar -expressions $expressions;
+E:\Users\Raffaele\Documents\GitHub\HandyPowershell\expressionsExecutor.ps1 -nParallelJobs $nPar -expressions $expressions;
