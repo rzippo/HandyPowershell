e59970b8f2d6ce66730bad188b2837d6fcf5a997
rzippo <raffaele.zippo@live.it>
2017-08-30 13:02:31 +0200

    Refactoring
    Also generalized the the execute test with relative paths

--- executorTest/buildExpressionsAndCallExecutor.ps1
+++ executorTest/buildExpressionsAndCallExecutor.ps1
@@ -1,15 +1,16 @@
-$target = "E:\Users\Raffaele\Desktop\test";
-$executionTarget = "E:\Users\Raffaele\Documents\GitHub\HandyPowershell\executorTest\executionTarget.ps1";
+$targetFolder = "~\Desktop";
+$executionTarget = ".\executionTarget.ps1";
 $numFiles = 30;
-$nPar = 5;
+$nParallelJobs = 5;
 $sleepTime = 3;
 
 $expressions = New-Object System.Collections.ArrayList;
+$executionTargetFullPath = (gci $executionTarget).FullName;
 
 foreach( $i in (1..$numFiles) )
 {
 	$filename = ( -join ((65..90) | Get-Random -Count 5 | % {[char]$_}) ) + ".txt";
-	$expressions.Add("$executionTarget -pwd $target -filename $filename -sleepTime $sleepTime") | Out-Null;
+	$expressions.Add("$executionTargetFullPath -targetFolder $targetFolder -filename $filename -sleepTime $sleepTime") | Out-Null;
 }
 
-E:\Users\Raffaele\Documents\GitHub\HandyPowershell\expressionsExecutor.ps1 -nParallelJobs $nPar -expressions $expressions;
+Execute-Expressions -nParallelJobs $nParallelJobs -expressions $expressions;
--- executorTest/executionTarget.ps1
+++ executorTest/executionTarget.ps1
@@ -1,9 +1,9 @@
 param(
-	[string]$pwd,
+	[string]$targetFolder,
 	[string]$filename,
 	[Int32]$sleepTime
 )
 
 Start-Sleep -s $sleepTime;
-cd $pwd;
+cd (Get-Item $targetFolder).FullName;
 "You know, stuff." | Out-File -FilePath $filename;
